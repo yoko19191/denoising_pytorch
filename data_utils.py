b7ae1c809a8c2ff8109529f309517a8de1e23d22
Guanghang Chen <guchen@utu.fi>
2023-03-30 05:55:45 +0800

    update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -1,8 +1,17 @@
+import os
+
 import numpy as np
+from scipy.stats import poisson, norm
+
 import matplotlib.pyplot as plt
+
+import cv2
+
 from skimage.transform import radon, iradon, iradon_sart
 
 from torch.utils.data import random_split
+
+
 
 ## visualization func
 def show_images_grid(images, figsize=(10, 10), cmap=None, suptitle=None):
@@ -26,9 +35,78 @@ def show_images_grid(images, figsize=(10, 10), cmap=None, suptitle=None):
     
     plt.subplots_adjust(wspace=0.1, hspace=0.1)
     plt.show()
+
     
+def show_error_profile(image_ref, image_target, suptitle=None):
+    """show pair-image and its diff profile
+    """
+    image_diff = cv2.absdiff(image_ref, image_target)
+    # visualization
+    fig, axs = plt.subplots(3, 3, figsize=(30, 20))
+    axs[0, 0].imshow(image_ref, cmap='gray')
+    axs[0, 0].set_title("image_ref")
+    axs[0, 1].imshow(image_target, cmap='gray')
+    axs[0, 1].set_title("image_target")
+    axs[0, 2].imshow(image_diff, cmap='gray')
+    axs[0, 2].set_title("error")
+    
+    axs[1, 0].hist(image_ref.ravel(), 256, [0, 256])
+    (mu, sigma) = norm.fit(image_ref.ravel())
+    axs[1, 0].set_title(f"pixel hist(mu:{mu:.3f} sigma:{sigma:.3f})")
+    axs[1, 0].set_xlabel('Pixel Value')
+    axs[1, 0].set_ylabel('Frequency')
+    
+    axs[1, 1].hist(image_target.ravel(), 256, [0, 256])
+    (mu, sigma) = norm.fit(image_target.ravel())
+    axs[1, 1].set_title(f"pixel hist(mu:{mu:.3f} sigma:{sigma:.3f})")
+    axs[1, 1].set_xlabel('Pixel Value')
+    axs[1, 1].set_ylabel('Frequency')
+    
+    axs[1, 2].hist(image_diff.ravel(), 256, [0, 256])
+    (mu, sigma) = norm.fit(image_diff.ravel())
+    axs[1, 2].set_title(f"pixel hist(mu:{mu:.3f} sigma:{sigma:.3f})")
+    axs[1, 2].set_xlabel('Pixel Value')
+    axs[1, 2].set_ylabel('Frequency')
+    
+    f = np.fft.fft2(image_ref)
+    fshift = np.fft.fftshift(f)
+    magnitude_spectrum = 20*np.log(np.abs(fshift))
+    im = axs[2, 0].imshow(magnitude_spectrum, cmap='gray')
+    axs[2, 0].set_title('Magnitude Spectrum')
+    axs[2, 0].set_xlabel('Frequency (Hz)')
+    axs[2, 0].set_ylabel('Frequency (Hz)')
+    fig.colorbar(im, ax=axs[2, 0], label='Magnitude (dB)')
+    
+    f = np.fft.fft2(image_target)
+    fshift = np.fft.fftshift(f)
+    magnitude_spectrum = 20*np.log(np.abs(fshift))
+    im = axs[2, 1].imshow(magnitude_spectrum, cmap='gray')
+    axs[2, 1].set_title('Magnitude Spectrum')
+    axs[2, 1].set_xlabel('Frequency (Hz)')
+    axs[2, 1].set_ylabel('Frequency (Hz)')
+    fig.colorbar(im, ax=axs[2, 1], label='Magnitude (dB)')
+    
+    f = np.fft.fft2(image_diff)
+    fshift = np.fft.fftshift(f)
+    magnitude_spectrum = 20*np.log(np.abs(fshift))
+    im = axs[2, 2].imshow(magnitude_spectrum, cmap='gray')
+    axs[2, 2].set_title('Magnitude Spectrum')
+    axs[2, 2].set_xlabel('Frequency (Hz)')
+    axs[2, 2].set_ylabel('Frequency (Hz)')
+    fig.colorbar(im, ax=axs[2, 2], label='Magnitude (dB)')
+    
+    fig.suptitle(suptitle)
 
 
+def show_prepared_image(output_dir, num_image=16, suptitle=None):
+    """show prepared images
+    """
+    filenames = [f for f in os.listdir(output_dir) if f.endswith(('.png', '.jpg', '.jpeg', '.bmp'))]
+    print(f"filenames len: {len(filenames)}")
+    files_path = [os.path.join(output_dir, x) for x in np.random.choice(filenames, num_image)]
+    images = [cv2.imread(x, 0) for x in files_path]
+    show_images_grid(images, cmap='gray', figsize=(15, 15), suptitle=suptitle)
+    
 ## experimental func
 def add_gaussian_noise(img, mean=0, std_dev=20):
     """ add gaussian noise to image, assume image is uint8
@@ -53,13 +131,7 @@ def add_poisson_noise(image, lam = 1.0, scale=1.0):
 
 
 ## dataset
-def split_dataset(dataset, train_ratio=0.8, val_ratio=0.1, test_ratio=0.1):
-    dataset_size = len(dataset)
-    train_size = int(train_ratio * dataset_size)
-    val_size = int(val_ratio * dataset_size)
-    test_size = dataset_size - train_size - val_size
-    train_data, val_data, test_data = random_split(dataset, [train_size, val_size, test_size])
-    return train_data, val_data, test_data
+
 
 
 ## projection func
